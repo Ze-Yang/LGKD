5eea53a019d57b67c173ce9647e25ab9948e57c5
zeyang <ze001@e.ntu.edu.sg>
2022-08-10 17:30:14 +0800

    update Image.xxx to InterpolationMode.xxx
    remove unused transforms

--- dataset/transform.py
+++ dataset/transform.py
@@ -4,17 +4,17 @@ import random
 import numbers
 import numpy as np
 import collections
-from PIL import Image
 import warnings
 import math
+from torchvision.transforms import InterpolationMode
 
 _pil_interpolation_to_str = {
-    Image.NEAREST: 'PIL.Image.NEAREST',
-    Image.BILINEAR: 'PIL.Image.BILINEAR',
-    Image.BICUBIC: 'PIL.Image.BICUBIC',
-    Image.LANCZOS: 'PIL.Image.LANCZOS',
-    Image.HAMMING: 'PIL.Image.HAMMING',
-    Image.BOX: 'PIL.Image.BOX',
+    InterpolationMode.NEAREST: 'PIL.Image.NEAREST',
+    InterpolationMode.BILINEAR: 'PIL.Image.BILINEAR',
+    InterpolationMode.BICUBIC: 'PIL.Image.BICUBIC',
+    InterpolationMode.LANCZOS: 'PIL.Image.LANCZOS',
+    InterpolationMode.HAMMING: 'PIL.Image.HAMMING',
+    InterpolationMode.BOX: 'PIL.Image.BOX',
 }
 
 
@@ -61,7 +61,7 @@ class Resize(object):
             ``PIL.Image.BILINEAR``
     """
 
-    def __init__(self, size, interpolation=Image.BILINEAR):
+    def __init__(self, size, interpolation=InterpolationMode.BILINEAR):
         assert isinstance(size, int) or (isinstance(size, collections.Iterable) and len(size) == 2)
         self.size = size
         self.interpolation = interpolation
@@ -75,7 +75,7 @@ class Resize(object):
             PIL Image: Rescaled image.
         """
         if lbl is not None:
-            return F.resize(img, self.size, self.interpolation), F.resize(lbl, self.size, Image.NEAREST)
+            return F.resize(img, self.size, self.interpolation), F.resize(lbl, self.size, InterpolationMode.NEAREST)
         else:
             return F.resize(img, self.size, self.interpolation)
 
@@ -116,149 +116,6 @@ class CenterCrop(object):
         return self.__class__.__name__ + f'(size={self.size})'
 
 
-class Pad(object):
-    """Pad the given PIL Image on all sides with the given "pad" value.
-    Args:
-        padding (int or tuple): Padding on each border. If a single int is provided this
-            is used to pad all borders. If tuple of length 2 is provided this is the padding
-            on left/right and top/bottom respectively. If a tuple of length 4 is provided
-            this is the padding for the left, top, right and bottom borders
-            respectively.
-        fill (int): Pixel fill value for constant fill. Default is 0.
-            This value is only used when the padding_mode is constant
-        padding_mode (str): Type of padding. Should be: constant, edge, reflect or symmetric.
-            Default is constant.
-            - constant: pads with a constant value, this value is specified with fill
-            - edge: pads with the last value at the edge of the image
-            - reflect: pads with reflection of image without repeating the last value on the edge
-                For example, padding [1, 2, 3, 4] with 2 elements on both sides in reflect mode
-                will result in [3, 2, 1, 2, 3, 4, 3, 2]
-            - symmetric: pads with reflection of image repeating the last value on the edge
-                For example, padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
-                will result in [2, 1, 1, 2, 3, 4, 4, 3]
-    """
-
-    def __init__(self, padding, fill=0, padding_mode='constant'):
-        assert isinstance(padding, (numbers.Number, tuple))
-        assert isinstance(fill, (numbers.Number, str))
-        assert padding_mode in ['constant', 'edge', 'reflect', 'symmetric']
-        if isinstance(padding, collections.Sequence) and len(padding) not in [2, 4]:
-            raise ValueError("Padding must be an int or a 2, or 4 element tuple, not a " +
-                             "{} element tuple".format(len(padding)))
-
-        self.padding = padding
-        self.fill = fill
-        self.padding_mode = padding_mode
-
-    def __call__(self, img, lbl=None):
-        """
-        Args:
-            img (PIL Image): Image to be padded.
-        Returns:
-            PIL Image: Padded image.
-        """
-        if lbl is not None:
-            return F.pad(img, self.padding, self.fill, self.padding_mode), F.pad(lbl, self.padding, self.fill, self.padding_mode)
-        else:
-            return F.pad(img, self.padding, self.fill, self.padding_mode)
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(padding={0}, fill={1}, padding_mode={2})'.\
-            format(self.padding, self.fill, self.padding_mode)
-
-
-class Lambda(object):
-    """Apply a user-defined lambda as a transform.
-    Args:
-        lambd (function): Lambda/function to be used for transform.
-    """
-
-    def __init__(self, lambd):
-        assert callable(lambd), repr(type(lambd).__name__) + " object is not callable"
-        self.lambd = lambd
-
-    def __call__(self, img, lbl=None):
-        if lbl is not None:
-            return self.lambd(img), self.lambd(lbl)
-        else:
-            return self.lambd(img)
-
-    def __repr__(self):
-        return self.__class__.__name__ + '()'
-
-
-class RandomRotation(object):
-    """Rotate the image by angle.
-
-    Args:
-        degrees (sequence or float or int): Range of degrees to select from.
-            If degrees is a number instead of sequence like (min, max), the range of degrees
-            will be (-degrees, +degrees).
-        resample ({PIL.Image.NEAREST, PIL.Image.BILINEAR, PIL.Image.BICUBIC}, optional):
-            An optional resampling filter.
-            See http://pillow.readthedocs.io/en/3.4.x/handbook/concepts.html#filters
-            If omitted, or if the image has mode "1" or "P", it is set to PIL.Image.NEAREST.
-        expand (bool, optional): Optional expansion flag.
-            If true, expands the output to make it large enough to hold the entire rotated image.
-            If false or omitted, make the output image the same size as the input image.
-            Note that the expand flag assumes rotation around the center and no translation.
-        center (2-tuple, optional): Optional center of rotation.
-            Origin is the upper left corner.
-            Default is the center of the image.
-    """
-
-    def __init__(self, degrees, resample=False, expand=False, center=None):
-        if isinstance(degrees, numbers.Number):
-            if degrees < 0:
-                raise ValueError("If degrees is a single number, it must be positive.")
-            self.degrees = (-degrees, degrees)
-        else:
-            if len(degrees) != 2:
-                raise ValueError("If degrees is a sequence, it must be of len 2.")
-            self.degrees = degrees
-
-        self.resample = resample
-        self.expand = expand
-        self.center = center
-
-    @staticmethod
-    def get_params(degrees):
-        """Get parameters for ``rotate`` for a random rotation.
-
-        Returns:
-            sequence: params to be passed to ``rotate`` for random rotation.
-        """
-        angle = random.uniform(degrees[0], degrees[1])
-
-        return angle
-
-    def __call__(self, img, lbl):
-        """
-            img (PIL Image): Image to be rotated.
-            lbl (PIL Image): Label to be rotated.
-
-        Returns:
-            PIL Image: Rotated image.
-            PIL Image: Rotated label.
-        """
-
-        angle = self.get_params(self.degrees)
-        if lbl is not None:
-            return F.rotate(img, angle, self.resample, self.expand, self.center), \
-                   F.rotate(lbl, angle, self.resample, self.expand, self.center)
-        else:
-            return F.rotate(img, angle, self.resample, self.expand, self.center)
-
-    def __repr__(self):
-        format_string = self.__class__.__name__ + '(degrees={0}'.format(self.degrees)
-        format_string += ', resample={0}'.format(self.resample)
-        format_string += ', expand={0}'.format(self.expand)
-        if self.center is not None:
-            format_string += ', center={0}'.format(self.center)
-        format_string += ')'
-        return format_string
-
-
 class RandomHorizontalFlip(object):
     """Horizontally flip the given PIL Image randomly with a given probability.
 
@@ -282,40 +139,6 @@ class RandomHorizontalFlip(object):
                 return F.hflip(img), F.hflip(lbl)
             else:
                 return F.hflip(img)
-        if lbl is not None:
-            return img, lbl
-        else:
-            return img
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(p={})'.format(self.p)
-
-
-class RandomVerticalFlip(object):
-    """Vertically flip the given PIL Image randomly with a given probability.
-
-    Args:
-        p (float): probability of the image being flipped. Default value is 0.5
-    """
-
-    def __init__(self, p=0.5):
-        self.p = p
-
-    def __call__(self, img, lbl):
-        """
-        Args:
-            img (PIL Image): Image to be flipped.
-            lbl (PIL Image): Label to be flipped.
-
-        Returns:
-            PIL Image: Randomly flipped image.
-            PIL Image: Randomly flipped label.
-        """
-        if random.random() < self.p:
-            if lbl is not None:
-                return F.vflip(img), F.vflip(lbl)
-            else:
-                return F.vflip(img)
         if lbl is not None:
             return img, lbl
         else:
@@ -388,96 +211,6 @@ class Normalize(object):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 
-class RandomCrop(object):
-    """Crop the given PIL Image at a random location.
-
-    Args:
-        size (sequence or int): Desired output size of the crop. If size is an
-            int instead of sequence like (h, w), a square crop (size, size) is
-            made.
-        padding (int or sequence, optional): Optional padding on each border
-            of the image. Default is 0, i.e no padding. If a sequence of length
-            4 is provided, it is used to pad left, top, right, bottom borders
-            respectively.
-        pad_if_needed (boolean): It will pad the image if smaller than the
-            desired size to avoid raising an exception.
-    """
-
-    def __init__(self, size, padding=0, pad_if_needed=False):
-        if isinstance(size, numbers.Number):
-            self.size = (int(size), int(size))
-        else:
-            self.size = size
-        self.padding = padding
-        self.pad_if_needed = pad_if_needed
-
-    @staticmethod
-    def get_params(img, output_size):
-        """Get parameters for ``crop`` for a random crop.
-
-        Args:
-            img (PIL Image): Image to be cropped.
-            output_size (tuple): Expected output size of the crop.
-
-        Returns:
-            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
-        """
-        w, h = img.size
-        th, tw = output_size
-        if w == tw and h == th:
-            return 0, 0, h, w
-
-        i = random.randint(0, h - th)
-        j = random.randint(0, w - tw)
-        return i, j, th, tw
-
-    def __call__(self, img, lbl=None):
-        """
-        Args:
-            img (PIL Image): Image to be cropped.
-            lbl (PIL Image): Label to be cropped.
-        Returns:
-            PIL Image: Cropped image.
-            PIL Image: Cropped label.
-        """
-        if lbl is None:
-            if self.padding > 0:
-                img = F.pad(img, self.padding)
-            # pad the width if needed
-            if self.pad_if_needed and img.size[0] < self.size[1]:
-                img = F.pad(img, padding=int((1 + self.size[1] - img.size[0]) / 2))
-            # pad the height if needed
-            if self.pad_if_needed and img.size[1] < self.size[0]:
-                img = F.pad(img, padding=int((1 + self.size[0] - img.size[1]) / 2))
-
-            i, j, h, w = self.get_params(img, self.size)
-
-            return F.crop(img, i, j, h, w)
-
-        else:
-            assert img.size == lbl.size, 'size of img and lbl should be the same. %s, %s' % (img.size, lbl.size)
-            if self.padding > 0:
-                img = F.pad(img, self.padding)
-                lbl = F.pad(lbl, self.padding)
-
-            # pad the width if needed
-            if self.pad_if_needed and img.size[0] < self.size[1]:
-                img = F.pad(img, padding=int((1 + self.size[1] - img.size[0]) / 2))
-                lbl = F.pad(lbl, padding=int((1 + self.size[1] - lbl.size[0]) / 2))
-
-            # pad the height if needed
-            if self.pad_if_needed and img.size[1] < self.size[0]:
-                img = F.pad(img, padding=int((1 + self.size[0] - img.size[1]) / 2))
-                lbl = F.pad(lbl, padding=int((1 + self.size[0] - lbl.size[1]) / 2))
-
-            i, j, h, w = self.get_params(img, self.size)
-
-            return F.crop(img, i, j, h, w), F.crop(lbl, i, j, h, w)
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
-
-
 class RandomResizedCrop(object):
     """Crop the given PIL Image to random size and aspect ratio.
     A crop of random size (default: of 0.08 to 1.0) of the original size and a random
@@ -491,7 +224,7 @@ class RandomResizedCrop(object):
         interpolation: Default: PIL.Image.BILINEAR
     """
 
-    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=InterpolationMode.BILINEAR):
         if isinstance(size, tuple):
             self.size = size
         else:
@@ -554,7 +287,7 @@ class RandomResizedCrop(object):
         i, j, h, w = self.get_params(img, self.scale, self.ratio)
         if lbl is not None:
             return F.resized_crop(img, i, j, h, w, self.size, self.interpolation), \
-                   F.resized_crop(lbl, i, j, h, w, self.size, Image.NEAREST)
+                   F.resized_crop(lbl, i, j, h, w, self.size, InterpolationMode.NEAREST)
         else:
             return F.resized_crop(img, i, j, h, w, self.size, self.interpolation)
 
@@ -564,100 +297,4 @@ class RandomResizedCrop(object):
         format_string += ', scale={0}'.format(tuple(round(s, 4) for s in self.scale))
         format_string += ', ratio={0}'.format(tuple(round(r, 4) for r in self.ratio))
         format_string += ', interpolation={0})'.format(interpolate_str)
-        return format_string
-
-
-class ColorJitter(object):
-    """Randomly change the brightness, contrast and saturation of an image.
-    Args:
-        brightness (float or tuple of float (min, max)): How much to jitter brightness.
-            brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
-            or the given [min, max]. Should be non negative numbers.
-        contrast (float or tuple of float (min, max)): How much to jitter contrast.
-            contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
-            or the given [min, max]. Should be non negative numbers.
-        saturation (float or tuple of float (min, max)): How much to jitter saturation.
-            saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
-            or the given [min, max]. Should be non negative numbers.
-        hue (float or tuple of float (min, max)): How much to jitter hue.
-            hue_factor is chosen uniformly from [-hue, hue] or the given [min, max].
-            Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5.
-    """
-    def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
-        self.brightness = self._check_input(brightness, 'brightness')
-        self.contrast = self._check_input(contrast, 'contrast')
-        self.saturation = self._check_input(saturation, 'saturation')
-        self.hue = self._check_input(hue, 'hue', center=0, bound=(-0.5, 0.5),
-                                     clip_first_on_zero=False)
-
-    def _check_input(self, value, name, center=1, bound=(0, float('inf')), clip_first_on_zero=True):
-        if isinstance(value, numbers.Number):
-            if value < 0:
-                raise ValueError("If {} is a single number, it must be non negative.".format(name))
-            value = [center - value, center + value]
-            if clip_first_on_zero:
-                value[0] = max(value[0], 0)
-        elif isinstance(value, (tuple, list)) and len(value) == 2:
-            if not bound[0] <= value[0] <= value[1] <= bound[1]:
-                raise ValueError("{} values should be between {}".format(name, bound))
-        else:
-            raise TypeError("{} should be a single number or a list/tuple with lenght 2.".format(name))
-
-        # if value is 0 or (1., 1.) for brightness/contrast/saturation
-        # or (0., 0.) for hue, do nothing
-        if value[0] == value[1] == center:
-            value = None
-        return value
-
-    @staticmethod
-    def get_params(brightness, contrast, saturation, hue):
-        """Get a randomized transform to be applied on image.
-        Arguments are same as that of __init__.
-        Returns:
-            Transform which randomly adjusts brightness, contrast and
-            saturation in a random order.
-        """
-        transforms = []
-
-        if brightness is not None:
-            brightness_factor = random.uniform(brightness[0], brightness[1])
-            transforms.append(Lambda(lambda img: F.adjust_brightness(img, brightness_factor)))
-
-        if contrast is not None:
-            contrast_factor = random.uniform(contrast[0], contrast[1])
-            transforms.append(Lambda(lambda img: F.adjust_contrast(img, contrast_factor)))
-
-        if saturation is not None:
-            saturation_factor = random.uniform(saturation[0], saturation[1])
-            transforms.append(Lambda(lambda img: F.adjust_saturation(img, saturation_factor)))
-
-        if hue is not None:
-            hue_factor = random.uniform(hue[0], hue[1])
-            transforms.append(Lambda(lambda img: F.adjust_hue(img, hue_factor)))
-
-        random.shuffle(transforms)
-        transform = Compose(transforms)
-
-        return transform
-
-    def __call__(self, img, lbl=None):
-        """
-        Args:
-            img (PIL Image): Input image.
-        Returns:
-            PIL Image: Color jittered image.
-        """
-        transform = self.get_params(self.brightness, self.contrast,
-                                    self.saturation, self.hue)
-        if lbl is not None:
-            return transform(img), lbl
-        else:
-            return transform(img)
-
-    def __repr__(self):
-        format_string = self.__class__.__name__ + '('
-        format_string += 'brightness={0}'.format(self.brightness)
-        format_string += ', contrast={0}'.format(self.contrast)
-        format_string += ', saturation={0}'.format(self.saturation)
-        format_string += ', hue={0})'.format(self.hue)
         return format_string
