357959ba010a7580053d4757c49102216470a79d
zeyang <ze001@e.ntu.edu.sg>
2024-01-17 22:24:18 +0800

    update download_voc.sh

--- data/download_voc.sh
+++ data/download_voc.sh
@@ -2,15 +2,18 @@
 
 # use this script in the destination folder.
 
+mkdir PascalVOC2012
+cd PascalVOC2012
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
 tar -xf VOCtrainval_11-May-2012.tar
-mkdir PascalVOC12
-mv VOCdevkit/VOC2012/* PascalVOC12
-cd PascalVOC12
+rm VOCtrainval_11-May-2012.tar
+
 wget http://cs.jhu.edu/~cxliu/data/SegmentationClassAug.zip
 wget http://cs.jhu.edu/~cxliu/data/SegmentationClassAug_Visualization.zip
 wget http://cs.jhu.edu/~cxliu/data/list.zip
 unzip SegmentationClassAug.zip
 unzip SegmentationClassAug_Visualization.zip
 unzip list.zip
-mv list splits
+rm SegmentationClassAug.zip
+rm SegmentationClassAug_Visualization.zip
+rm list.zip
