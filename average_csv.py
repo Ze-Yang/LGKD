1458729b48df38f71681a3489428b86ca1bbbe57
zeyang <ze001@e.ntu.edu.sg>
2024-01-26 01:30:14 +0800

    update result summary average_csv.py

--- average_csv.py
+++ average_csv.py
@@ -1,40 +1,44 @@
 import sys
+import csv
 
 path = sys.argv[1]
 
-c, s = 0, 0.
+mIoU_all_sum = 0.
 
-avg_first = 0.
-avg_last = 0.
-nb_first_classes = 0
-col_index = -1
+bg = 0.
+avg_old = 0.
+avg_new = 0.
+num_old_classes = 0
+col_index = 0
 
 with open(path, 'r') as f:
-    for line_index, line in enumerate(f):
-        split = line.split(',')
-        a = split[-1]
-        a = float(a)
-        s += a
-        c += 1
-        step = line.split(',')[0]
+    data = list(csv.reader(f))
+    num_rows = len(data)
+    for step, x in enumerate(data):
+        mIoU_all = float(x[-1])
+        mIoU_all_sum += mIoU_all
+        assert step == int(x[0]), 'Results for step {} missed.'.format(step)
 
-        if line_index == 0:
-            for col_index in range(1, len(split)):
-                if split[col_index] == "x": break
-        elif col_index > -1:
-            if len(split[1:col_index]) == 0:
-                avg_first = 0.
+        if step == 0:
+            for col_index in range(1, len(x)):
+                if x[col_index] in ('x', 'X'):
+                    break
+            num_old_classes = col_index - 2
+            num_new_classes = len(x) - num_old_classes - 3
+        elif step == num_rows - 1:
+            if len(x[2:col_index]) == 0:
+                avg_old = 0.
             else:
-                avg_first = sum([float(i) for i in split[1:col_index] if i not in ('x', 'X')]) / len(split[1:col_index])
-            if len(split[col_index:-1]) == 0:
-                avg_last = 0.
+                avg_old = sum([float(i) for i in x[2:col_index]]) / num_old_classes
+            if len(x[col_index:-1]) == 0:
+                avg_new = 0.
             else:
-                avg_last = sum([float(i) for i in split[col_index:-1] if i not in ('x', 'X')]) / len(split[col_index:-1])
+                avg_new = sum([float(i) for i in x[col_index:-1]]) / num_new_classes
+            bg = float(x[1])
 
-
-
-print(f"Last Step: {step}")
-print(f"Final Mean IoU {round(100 * a, 2)}")
-print(f'Average Mean IoU {round(100 * s / c, 2)}')
-print(f'Mean IoU first {round(100 * avg_first, 2)}')
-print(f'Mean IoU last {round(100 * avg_last, 2)}')
+print("{:<12}: {:>5d}".format("Final Step", step))
+print("{:<12}: {:>2.2f}".format("IoU BG", 100 * bg))
+print("{:<12}: {:>2.2f}".format("mIoU old", 100 * avg_old))
+print("{:<12}: {:>2.2f}".format("mIoU new", 100 * avg_new))
+print("{:<12}: {:>2.2f}".format("mIoU all", 100 * mIoU_all))
+print("{:<12}: {:>2.2f}".format("mIoU Avg", 100 * mIoU_all_sum / num_rows))
